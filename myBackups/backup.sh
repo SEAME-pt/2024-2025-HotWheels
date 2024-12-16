6c09816e1d4b5b211738e9bb06600789cc3b6c4e
Michel Batista <88142367+MicchelFAB@users.noreply.github.com>
2024-12-16 10:34:29 +0000

    Automating the Backup.

--- myBackups/backup.sh
+++ myBackups/backup.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
 # Variables
-BACKUP_DIR="/home/michel/Documents/myScripts/backups"  # Directory where backups will be stored
+BACKUP_DIR="$HOME/Documents/myScripts/backups"  # Directory where backups will be stored
+
+# Make sure the backup directory exists
+mkdir -p $BACKUP_DIR
 DATE=$(date +"%Y%m%d%H")                    # Date format for unique backup names
 PARTITION_TABLE_FILE="${BACKUP_DIR}/partition_table_backup_${DATE}.txt"  # Partition table backup filename
 BACKUP_IMG="${BACKUP_DIR}/backup_${DATE}.img"   # Backup image filename
 
-# Partition to be backed up (adjust if needed)
-if [ -z "$1" ]; then
-	echo "Usage: $0 <source_device>"
-	exit 1
-fi
+# List connected devices
+echo "Connected devices:"
+devices=($(lsblk -d -o NAME | grep -E 'sd|mmcblk'))
+for i in "${!devices[@]}"; do
+    echo "[$i] ${devices[$i]}"
+done
 
-SOURCE_DEVICE="$1"                         # Partition or disk to back up
+# Prompt user to select a device
+read -p "Enter the number corresponding to the device you want to back up: " DEVICE_INDEX
+SOURCE_DEVICE="${devices[$DEVICE_INDEX]}"
+
+# List partitions of the selected device
+echo "Partitions on /dev/$SOURCE_DEVICE:"
+partitions=($(lsblk -ln -o NAME | grep "^${SOURCE_DEVICE}"))
+for i in "${!partitions[@]}"; do
+    echo "[$i] ${partitions[$i]}"
+done
+
+# Prompt user to select a partition
+read -p "Enter the number corresponding to the partition you want to back up: " PARTITION_INDEX
+SOURCE_PARTITION="${partitions[$PARTITION_INDEX]}"
 
 # Unmount all partitions of the device
 echo "Unmounting all partitions of $SOURCE_DEVICE..."
 sudo umount /dev/${SOURCE_DEVICE}*
-
-SOURCE_PARTITION="${SOURCE_DEVICE}1"                   # Partition or disk to back up
-PARTITION_TYPE="ext4"                           # Filesystem type (adjust accordingly)
 
 # Make sure the backup directory exists
 mkdir -p $BACKUP_DIR
@@ -34,7 +48,7 @@ echo "Starting system backup..."
 sudo partclone.ext4 -c -s /dev/$SOURCE_PARTITION -o $BACKUP_IMG
 
 # Optional: Remove backups older than 7 days
-find $BACKUP_DIR -type f -name "*.img" -mtime +7 -exec rm {} \;
+find $BACKUP_DIR -type f -name "*.img" -mtime +15 -exec rm {} \;
 
 # Step 3: Log the completion of the backup
 echo "Backup completed at $DATE now you can use the device"
