3ba5d46f3f8fe7349ddf2608fa394fd81f6b0d3f
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-01-13 10:57:45 +0000

    updating services scripts

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Check the status of 'mini_display_info.service' and display only the active status part
+echo "Checking the status of 'mini_display_info.service'..."
+sudo systemctl status mini_display_info.service | grep -A 8 'mini_display_info.service - Mini Display Info'
+
 # Check the status of 'check_api.service' and display only the active status part
 echo "Checking the status of 'check_api.service'..."
 sudo systemctl status check_api.service | grep -A 8 'check_api.service - Check Github API Service'
