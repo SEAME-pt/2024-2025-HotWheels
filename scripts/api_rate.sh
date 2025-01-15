38d53dcd37d067781594f591129c42c4565402be
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-01-15 16:04:14 +0000

    erasing scripts folder

--- scripts/api_rate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-GITHUB_TOKEN=""
-
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/rate_limit | grep -A 5 '"rate": {'
