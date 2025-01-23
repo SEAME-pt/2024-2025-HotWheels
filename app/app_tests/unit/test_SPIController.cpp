fdf1ee76037e148baa273c6eb1dbfd799a48d2dc
Felix LE BIHAN <xilef.nahibel@gmail.com>
2025-01-23 16:15:03 +0000

    Improved SPIController tests

--- app/app_tests/unit/test_SPIController.cpp
+++ app/app_tests/unit/test_SPIController.cpp
@@ -1,46 +1,95 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "MockSPI.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 using ::testing::_;
 using ::testing::Return;
 using ::testing::Throw;
 
-class SPIControllerTest : public ::testing::Test {
+class SPIControllerTest : public ::testing::Test
+{
 protected:
     MockSPI mockSPI;
 };
 
-TEST_F(SPIControllerTest, OpenDeviceSuccess) {
+// Open Device Tests
+TEST_F(SPIControllerTest, OpenDeviceSuccess)
+{
     EXPECT_CALL(mockSPI, openDevice("/dev/spidev0.0")).WillOnce(Return(true));
     ASSERT_TRUE(mockSPI.openDevice("/dev/spidev0.0"));
 }
 
-TEST_F(SPIControllerTest, OpenDeviceFailure) {
+TEST_F(SPIControllerTest, OpenDeviceFailure)
+{
     EXPECT_CALL(mockSPI, openDevice("/nonexistent/spidev0.0")).WillOnce(Return(false));
     ASSERT_FALSE(mockSPI.openDevice("/nonexistent/spidev0.0"));
 }
 
-TEST_F(SPIControllerTest, ConfigureSPI) {
+// Configure SPI Tests
+TEST_F(SPIControllerTest, ConfigureSPIValidParameters)
+{
     EXPECT_CALL(mockSPI, configure(0, 8, 500000)).Times(1);
     ASSERT_NO_THROW(mockSPI.configure(0, 8, 500000));
 }
 
-TEST_F(SPIControllerTest, WriteByteSuccess) {
+TEST_F(SPIControllerTest, ConfigureSPIInvalidParameters)
+{
+    EXPECT_CALL(mockSPI, configure(_, _, _)).Times(0);
+    ASSERT_NO_THROW(mockSPI.configure(5, 8, 500000)); // Invalid mode, no action expected
+}
+
+// Write Byte Tests
+TEST_F(SPIControllerTest, WriteByteSuccess)
+{
     EXPECT_CALL(mockSPI, writeByte(0x01, 0xFF)).Times(1);
     ASSERT_NO_THROW(mockSPI.writeByte(0x01, 0xFF));
 }
 
-TEST_F(SPIControllerTest, ReadByteSuccess) {
+// Read Byte Tests
+TEST_F(SPIControllerTest, ReadByteSuccess)
+{
     EXPECT_CALL(mockSPI, readByte(0x01)).WillOnce(Return(0xFF));
     uint8_t value = mockSPI.readByte(0x01);
     ASSERT_EQ(value, 0xFF);
 }
 
-TEST_F(SPIControllerTest, SpiTransferSuccess) {
+// SPI Transfer Tests
+TEST_F(SPIControllerTest, SpiTransferSuccess)
+{
     uint8_t tx[] = {0x02, 0x01, 0xFF};
     uint8_t rx[sizeof(tx)] = {0};
 
     EXPECT_CALL(mockSPI, spiTransfer(tx, rx, sizeof(tx))).Times(1);
     ASSERT_NO_THROW(mockSPI.spiTransfer(tx, rx, sizeof(tx)));
+}
+
+TEST_F(SPIControllerTest, SpiTransferWithNullTx)
+{
+    uint8_t *tx = nullptr;
+    uint8_t rx[3] = {0};
+
+    EXPECT_CALL(mockSPI, spiTransfer(_, _, _)).Times(0);
+    ASSERT_NO_THROW(mockSPI.spiTransfer(tx, rx, 3)); // No action expected
+}
+
+TEST_F(SPIControllerTest, SpiTransferWithNullRx)
+{
+    uint8_t tx[3] = {0x02, 0x01, 0xFF};
+    uint8_t *rx = nullptr;
+
+    EXPECT_CALL(mockSPI, spiTransfer(_, _, _)).Times(0);
+    ASSERT_NO_THROW(mockSPI.spiTransfer(tx, rx, 3)); // No action expected
+}
+
+// Close Device Tests
+TEST_F(SPIControllerTest, CloseDeviceSuccess)
+{
+    EXPECT_CALL(mockSPI, closeDevice()).Times(1);
+    ASSERT_NO_THROW(mockSPI.closeDevice());
+}
+
+TEST_F(SPIControllerTest, CloseDeviceWithoutOpening)
+{
+    EXPECT_CALL(mockSPI, closeDevice()).Times(0);
+    ASSERT_NO_THROW(mockSPI.closeDevice()); // No action expected
 }
--- app/includes/canbus/SPIController.hpp
+++ app/includes/canbus/SPIController.hpp
@@ -1,12 +1,14 @@
 #ifndef SPICONTROLLER_HPP
 #define SPICONTROLLER_HPP
 
-#include <string>
 #include <cstdint>
+#include <string>
 
 class SPIController
 {
 public:
+    enum class Opcode : uint8_t { Write = 0x02, Read = 0x03 };
+
     SPIController();
     ~SPIController();
 
@@ -22,6 +24,10 @@ private:
     uint8_t mode;
     uint8_t bits;
     uint32_t speed;
+
+    static constexpr uint8_t DefaultBitsPerWord = 8;
+    static constexpr uint32_t DefaultSpeedHz = 1'000'000;
+    static constexpr uint8_t DefaultMode = 0;
 };
 
 #endif // SPICONTROLLER_HPP
