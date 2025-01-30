2567440ac7b4e6bbbdc82cd8aa1ab048172f22fe
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-01-30 14:34:56 +0000

    changing servo angle to have the same turning ratio

--- car_controls/includes/EngineController.hpp
+++ car_controls/includes/EngineController.hpp
@@ -13,7 +13,7 @@ class EngineController : public QObject
 private:
     const int MAX_ANGLE = 180;
     const int SERVO_CENTER_PWM = 345;
-    const int SERVO_LEFT_PWM = 345 - 90;
+    const int SERVO_LEFT_PWM = 345 - 120;
     const int SERVO_RIGHT_PWM = 345 + 120;
     const int STEERING_CHANNEL = 0;
 
