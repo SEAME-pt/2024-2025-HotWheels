e2d01709daa81d9458b62e30c3eff8616512f12b
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-02-06 16:57:03 +0000

    Refactor CarDataI to use separate mutexes for joystick and temperature handling; add thread pool for concurrent request processing

--- car_controls/ZeroC/server.cpp
+++ car_controls/ZeroC/server.cpp
@@ -7,30 +7,32 @@ class CarDataI : public Data::CarData {
 private:
     bool joystick_enable;
     double car_temperature;
-    std::mutex mutex;
+
+    std::mutex joystick_mutex;
+    std::mutex temperature_mutex;
 
 public:
     CarDataI() : joystick_enable(true), car_temperature(0.0) {}
 
     void setJoystickValue(bool newValue, const Ice::Current&) override {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(joystick_mutex);
         joystick_enable = newValue;
         std::cout << "Joystick value set to: " << joystick_enable << std::endl;
     }
 
     bool getJoystickValue(const Ice::Current&) override {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(joystick_mutex);
         return joystick_enable;
     }
 
     void setCarTemperatureValue(double newValue, const Ice::Current&) override {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(temperature_mutex);
         car_temperature = newValue;
         std::cout << "Car temperature value set to: " << car_temperature << std::endl;
     }
 
     double getCarTemperatureValue(const Ice::Current&) override {
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(temperature_mutex);
         return car_temperature;
     }
 };
@@ -40,6 +42,9 @@ int main(int argc, char* argv[]) {
         Ice::CommunicatorPtr communicator = Ice::initialize(argc, argv);
         Ice::ObjectAdapterPtr adapter = communicator->createObjectAdapterWithEndpoints(
             "CarDataAdapter", "tcp -h 127.0.0.1 -p 10000");
+
+        // Create a thread pool to allow concurrent request processing
+        adapter->addServantLocator(new Ice::ThreadPool(10));
 
         CarDataI* servant = new CarDataI();
         adapter->add(servant, Ice::stringToIdentity("carData"));
