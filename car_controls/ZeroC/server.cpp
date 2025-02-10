15ae4abcc9754750d170b02125832021e088fd2b
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-02-10 16:34:23 +0000

    Remove deprecated ZeroC server and client implementation along with related files

--- car_controls/ZeroC/server.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "CarDataI.hpp"  // Include the header for CarDataI class
-
-int main(int argc, char* argv[]) {
-    Data::CarDataI carDataServer(argc, argv);  // Create an instance of CarDataI
-    return 0;
-}
