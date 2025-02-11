c091fcb56cd26973bd4c654caeebcf23567d8a37
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-02-11 11:54:47 +0000

    Refactor main function to create ControlsManager on the stack

--- car_controls/sources/main.cpp
+++ car_controls/sources/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
   std::signal(SIGINT, handleSigint);
 
-  /* try {
+  try {
     ControlsManager *m_controlsManager;
     m_controlsManager = new ControlsManager(argc, argv);
     return a.exec();
@@ -23,19 +23,5 @@ int main(int argc, char *argv[]) {
     std::cerr << "Error: " << e.what() << std::endl;
     return 1;
   }
-  std::cout << "Service has stopped." << std::endl; */
-
-  try {
-    // Create ControlsManager on the STACK instead of HEAP
-    ControlsManager m_controlsManager(argc, argv);
-
-    // Run the application event loop
-    int result = a.exec();
-
-    qDebug() << "Service has stopped.";
-    return result;
-  } catch (const std::exception &e) {
-      std::cerr << "Error: " << e.what() << std::endl;
-      return 1;
-  }
+  std::cout << "Service has stopped." << std::endl;
 }
