1ea0c477fce0443693be1d396371cffe7fe3fe0e
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-03-20 15:26:26 +0000

    Fix conditional structure for joystick value handling in ControlsManager

--- apps/car_controls/sources/ControlsManager.cpp
+++ apps/car_controls/sources/ControlsManager.cpp
@@ -89,10 +89,13 @@ ControlsManager::ControlsManager(int argc, char **argv, QObject *parent)
 
 			if (received_msg.find("joystick_value") == 0) {
 				std::string value = received_msg.substr(std::string("joystick_value ").length());
-				if (value == "true") {
+				if (value == "true")
+				{
 					qDebug() << "Received true";
 					setMode(DrivingMode::Manual);
-				} else (value == "false") {
+				}
+				else (value == "false")
+				{
 					qDebug() << "Received false";
 					setMode(DrivingMode::Automatic);
 				}
