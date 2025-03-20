531e247675067ee609c0d6847df3fa6397c05faf
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-03-20 15:27:25 +0000

    Fix conditional structure and improve debug logging in ControlsManager

--- apps/car_controls/sources/ControlsManager.cpp
+++ apps/car_controls/sources/ControlsManager.cpp
@@ -91,12 +91,12 @@ ControlsManager::ControlsManager(int argc, char **argv, QObject *parent)
 				std::string value = received_msg.substr(std::string("joystick_value ").length());
 				if (value == "true")
 				{
-					qDebug() << "Received true";
+					qDebug() << "Received true" << std::endl;
 					setMode(DrivingMode::Manual);
 				}
-				else (value == "false")
+				else if (value == "false")
 				{
-					qDebug() << "Received false";
+					qDebug() << "Received false" << std::endl;
 					setMode(DrivingMode::Automatic);
 				}
 			}
