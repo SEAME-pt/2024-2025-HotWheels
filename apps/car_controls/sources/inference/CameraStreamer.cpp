ef656616bd35ff2dbd5d32cd46f95e9807558077
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-05-08 14:04:26 +0100

    fix: Update published frame label from "inference_result" to "inference_frame" in CameraStreamer

--- apps/car_controls/sources/inference/CameraStreamer.cpp
+++ apps/car_controls/sources/inference/CameraStreamer.cpp
@@ -214,7 +214,7 @@ void CameraStreamer::start() {
 		stream.waitForCompletion();  // Synchronize
 
 		if (m_publisherObject) {
-			m_publisherObject->publishFrame("inference_result", d_resized_mask);  // Publish the frame
+			m_publisherObject->publishFrame("inference_frame", d_resized_mask);  // Publish the frame
 		}
 
 		uploadFrameToTexture(d_resized_mask);  // Upload final result to OpenGL
