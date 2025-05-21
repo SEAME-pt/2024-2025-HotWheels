4a49a453b00ead8d824f2c17a9068777b2cd381b
Tiago Pereira <tiagosodrepereira@gmail.com>
2025-05-21 09:22:53 +0100

    fix: Remove unnecessary GStreamer pipeline elements for improved performance

--- apps/car_controls/sources/inference/CameraStreamer.cpp
+++ apps/car_controls/sources/inference/CameraStreamer.cpp
@@ -177,8 +177,6 @@ void CameraStreamer::start() {
 		"video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! "
 		"nvvidconv ! "
 		"video/x-raw, format=RGBA ! "
-		"videoconvert ! "
-		"video/x-raw, format=BGR ! "
 		"appsink name=sink sync=false max-buffers=1 drop=true";
 
 	GstElement* pipeline = gst_parse_launch(pipelineStr.c_str(), nullptr);
