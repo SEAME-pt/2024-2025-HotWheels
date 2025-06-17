25724c96d7a67505f6c120277eedee8a845b00b6
Felix LE BIHAN <xilef.nahibel@gmail.com>
2025-06-17 11:04:00 +0100

    feat: Update CarManager to use semi-transparent background color

--- apps/cluster/sources/main/CarManager.cpp
+++ apps/cluster/sources/main/CarManager.cpp
@@ -55,6 +55,7 @@ CarManager::CarManager(int argc, char **argv, QWidget *parent)
     )";
     this->setStyleSheet(style);
     //this->centralWidget()->setStyleSheet("background-color: darkblue;");
+    this->centralWidget()->setStyleSheet("background-color: rgba(4, 36, 49, 255);");  // semi-transparent
 
     QPixmap test(":/images/background.jpg");
     qDebug() << "Image loaded from Qt resource?" << !test.isNull();
