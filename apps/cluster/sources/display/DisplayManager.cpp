80f38a7a132d53fb8752c8d5b6706e7c0db59ed5
Felix LE BIHAN <xilef.nahibel@gmail.com>
2025-06-18 10:45:51 +0100

    fix: Uncomment lane keeping assist and speed limit label logic in updateDrivingMode

--- apps/cluster/sources/display/DisplayManager.cpp
+++ apps/cluster/sources/display/DisplayManager.cpp
@@ -219,22 +219,22 @@ void DisplayManager::updateDrivingMode(DrivingMode newMode) {
 	switch (newMode) {
 	case DrivingMode::Manual:
 		modeText = "Manual";
-		/* m_ui->laneKeepingAssistLabel->show();
+		m_ui->laneKeepingAssistLabel->show();
 		m_ui->laneDepartureWarningLabel->show();
-		m_ui->speedLimit80Label->hide();
-		m_ui->speedLimit50Label->show();
-		// Stop blinking if activee
+		/* m_ui->speedLimit80Label->hide();
+		m_ui->speedLimit50Label->show(); */
+		// Stop blinking if active
 		if (m_blinkTimer) {
 			m_blinkTimer->stop();
 			m_blinkTimer->deleteLater();
 			m_blinkTimer = nullptr;
-		} */
+		}
 		break;
 	case DrivingMode::Automatic:
 		modeText = "Automatic";
-		/* m_ui->laneKeepingAssistLabel->hide();
-		m_ui->speedLimit50Label->hide();
-		m_ui->speedLimit80Label->show();
+		m_ui->laneKeepingAssistLabel->hide();
+		/* m_ui->speedLimit50Label->hide();
+		m_ui->speedLimit80Label->show(); */
 		if (!m_blinkTimer) {
 			m_blinkTimer = new QTimer(this);
 			connect(m_blinkTimer, &QTimer::timeout, this, [=]() {
@@ -242,7 +242,7 @@ void DisplayManager::updateDrivingMode(DrivingMode newMode) {
 				m_ui->laneDepartureWarningLabel->setVisible(!currentlyVisible);
 			});
 			m_blinkTimer->start(150);  // Blink every 150ms
-		} */
+		}
 		break;
 	}
 	m_ui->drivingModeLabel->setText(modeText);
